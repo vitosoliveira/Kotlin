cd4d33b810d79b3d60230e1a5cb812333d478c45
truethaisregina <truethaisregina@gmail.com>
2021-06-02 23:16:05 -0300

    Update ProdutoActivity.kt

--- app/src/main/java/br/com/thaisregina/lmsapp/ProdutoActivity.kt
+++ app/src/main/java/br/com/thaisregina/lmsapp/ProdutoActivity.kt
@@ -1,13 +1,16 @@
 package br.com.thaisregina.lmsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_produtos.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class ProdutoActivity: DebugActivity() {
     var produtos: Produtos? = null
-    override fun onCreate(savedInstanceState: Bundle?) {    
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_produtos)
 
@@ -34,4 +37,41 @@ class ProdutoActivity: DebugActivity() {
             })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_produtos, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item?.itemId
+        if  (id == R.id.action_remover) {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage("Deseja excluir a disciplina")
+                .setPositiveButton("Sim") {
+                        dialog, which ->
+                    dialog.dismiss()
+                    taskExcluir()
+                }.setNegativeButton("Não") {
+                        dialog, which -> dialog.dismiss()
+                }.create().show()
+        }
+        else if (id == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun taskExcluir() {
+        if (this.produtos != null && this.produtos is Produtos) {
+            // Thread para remover a disciplina
+            Thread {
+                ProdutosService.delete(this.produtos as Produtos)
+                runOnUiThread {
+                    // após remover, voltar para activity anterior
+                    finish()
+                }
+            }.start()
+        }
+    }
 }
